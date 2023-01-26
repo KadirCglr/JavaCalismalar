0bec066579478cfef30aba7f37e2abb500f955ca
TechnoStudy <ismettechno@gmail.com>
2023-01-26 17:48:50 +0000

    fori

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,5 +3,9 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("merhaba");
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("M");
+        }
     }
 }
